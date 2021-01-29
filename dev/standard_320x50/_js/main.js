5707c168be1b293a2ea75ac94d624f868048c3c8
Gar <gar@lonelydatu.com>
2021-01-29 17:11:47 -0500

    ON

--- dev/standard_320x50/_js/main.js
+++ dev/standard_320x50/_js/main.js
@@ -47,7 +47,7 @@ function start(){
 	tl.from(".cta", TIME, {opacity:0}, "+=.3")
 	
 	const tl_cta = new TimelineMax({repeat:5, yoyo:true})
-	tl_cta.to(".cta.chev", .3, {x:"+=4", ease:Power1.easeOut})
+	tl_cta.to(".cta.chev", .3, {x:"+=3", ease:Power1.easeOut})
 	tl.add(tl_cta)
 }
 
